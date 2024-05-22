51f4d621150bc5ccb23dd7ced0904afe66390a42
gabriel_rc__ <ganriel201@gmail.com>
2024-05-21 22:55:05 -0300

    metodo para remover nó da lista circular duplamente encadeada

--- estruturas/listacircularduplamenteencadeada/ListaCircularDuplamenteEncadeada.java
+++ estruturas/listacircularduplamenteencadeada/ListaCircularDuplamenteEncadeada.java
@@ -1,5 +1,10 @@
 package estruturas.listacircularduplamenteencadeada;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ListaCircularDuplamenteEncadeada<T> {
     private No<T> head = null;
     private No<T> tail = null;
@@ -19,10 +24,28 @@ public class ListaCircularDuplamenteEncadeada<T> {
         novoNo.setProximo(head);
         novoNo.setAnterior(tail);
 
-        head = novoNo;
+        this.setHead(novoNo);
 
         novoNo.getProximo().setAnterior(novoNo);
         tail.setProximo(novoNo);
     }
 
+    public T remover(No<T> noASerRemovido){
+        No<T> proximo = noASerRemovido.getProximo();
+        No<T> anterior = noASerRemovido.getAnterior();
+
+        if(noASerRemovido.equals(head)){
+            this.setHead(proximo);
+        } else if(noASerRemovido.equals(tail)){
+            this.setTail(anterior);
+        }
+
+        noASerRemovido.setProximo(null);
+        noASerRemovido.setAnterior(null);
+
+        proximo.setAnterior(anterior);
+        anterior.setProximo(proximo);
+
+        return noASerRemovido.getValor();
+    }
 }
